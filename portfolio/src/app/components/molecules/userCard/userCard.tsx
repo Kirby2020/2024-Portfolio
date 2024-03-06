8fa2c6e7189b8a67041a13316b157787878531ad
Kirby2019 <74135383+Kirby2020@users.noreply.github.com>
2024-03-06 10:17:49 +0100

    feat: Create userCard component

---
+++ portfolio/src/app/components/molecules/userCard/userCard.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import userImage from "../../../../../public/images/user-image.png";
+import styles from "./userCard.module.css";
+import { userSocials } from "@/app/lib/data/userSocials";
+import SocialsList from "../socialsList/socialsList";
+
+export default function UserCard() {
+    return (
+        <div className={styles.userCard}>
+            <div className={styles.userInfo}>
+                <div>
+                    <p>Jonathan</p>
+                    <p>21 years old</p>
+                    <p>Male</p>
+                </div>
+                <div>
+                    <p>Hobbies:</p>
+                    <ul>
+                        <li>Gaming</li>
+                        <li>Programming</li>
+                        <li>Drawing</li>
+                        <li>Violin</li>
+                        <li>Badminton</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className={styles.userInfo}>
+                <Image className={styles.userImage} src={userImage} alt="user image" />
+                <SocialsList socialIcons={userSocials} />
+            </div>
+        </div>
+    );
+}
