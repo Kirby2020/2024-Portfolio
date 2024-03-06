499c09f1ef2f0c1eb42103b51c689355e13b7c08
Kirby2019 <74135383+Kirby2020@users.noreply.github.com>
2024-03-06 10:18:35 +0100

    fix: Update header routes

--- portfolio/src/app/components/organisms/header/header.tsx
+++ portfolio/src/app/components/organisms/header/header.tsx
@@ -6,12 +6,12 @@ export default function Header() {
     <header className={styles.header}>
       <nav>
         <ul>
-          <Link href="/home">Home</Link>
+          <Link href="/">Home</Link>
           <Link href="/projects">Projects</Link>
           <Link href="/gallery">Gallery</Link>
           <Link href="/anime">Anime</Link>
           <Link href="/games">Games</Link>
-          <Link href="/aboutme">About Me</Link>
+          <Link href="/about-me">About Me</Link>
           <Link href="/blog">Blog</Link>
           <Link href="/contact">Contact</Link>
           <Link href="/login">Login</Link>
