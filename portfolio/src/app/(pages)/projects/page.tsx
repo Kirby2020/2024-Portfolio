12fb72352f99677e735927b1856610c39faca5d8
Kirby2019 <74135383+Kirby2020@users.noreply.github.com>
2024-03-07 16:41:23 +0100

    feat: Create projects page

---
+++ portfolio/src/app/(pages)/projects/page.tsx
@@ -0,0 +1,13 @@
+import PageHeader from "@/app/components/molecules/pageHeader/pageHeader";
+
+export default function Projects() {
+  return (
+    <>
+      <PageHeader title="Projects">
+        Along my journey, I’ve made a lot of different projects.
+        <br />
+        Here I will post all the large coding projects I have made.
+      </PageHeader>
+    </>
+  );
+}
