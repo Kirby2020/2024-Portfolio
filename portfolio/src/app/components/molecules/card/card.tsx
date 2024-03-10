8e6b6b91c65f0456893214cf8a4289fa8535824a
Kirby2019 <74135383+Kirby2020@users.noreply.github.com>
2024-03-10 16:30:41 +0100

    style: Remove card header styling
    Each card type will implement it's own header style

--- portfolio/src/app/components/molecules/card/card.tsx
+++ portfolio/src/app/components/molecules/card/card.tsx
@@ -11,9 +11,7 @@ interface Props {
 export default function Card(props: Props) {
   return (
     <div className={styles.card}>
-      {props.children && (
-        <div className={styles.cardHeader}>{props.children}</div>
-      )}
+      {props.children && <div>{props.children}</div>}
       <div className={styles.cardFooter}>
         <h3>{props.title}</h3>
         <p>{props.description}</p>
