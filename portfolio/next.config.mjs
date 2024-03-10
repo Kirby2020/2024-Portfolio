76b9f66dfe5fd1722a50b8cedf55bf4cdf64bd7b
Kirby2019 <74135383+Kirby2020@users.noreply.github.com>
2024-03-10 17:02:36 +0100

    build: add vercel storage public url to whitelist

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "k41jfyz0rmi8ju7o.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/*"
+      }
+    ]
+  }
+};
 
 export default nextConfig;
