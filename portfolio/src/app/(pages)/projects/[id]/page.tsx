c1eab1a9a5a20378079d245c025cac1532c8f8bf
Kirby2019 <74135383+Kirby2020@users.noreply.github.com>
2024-03-11 13:58:41 +0100

    feat: Create temp project page

---
+++ portfolio/src/app/(pages)/projects/[id]/page.tsx
@@ -0,0 +1,37 @@
+import styles from "./page.module.css";
+import { getProject } from "@/app/api/projects/projectsController";
+import PageHeader from "@/app/components/molecules/pageHeader/pageHeader";
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default async function Project({ params }: Props) {
+  const project = await getProject(params.id);
+
+  const callToActionText = project.type
+    ? "View on " + project.type
+    : "View here";
+
+  return (
+    <div className={styles.project}>
+      <PageHeader title={project.title}>
+        <Link href={project.url}>{callToActionText}</Link>
+      </PageHeader>
+
+      <div className={styles.projectInfo}>
+        <div className={styles.projectDescription}>
+          <h3>Description</h3>
+          <p>{project.description}</p>
+        </div>
+        <div className={styles.projectImage}>
+          <img src={project.previewUrl} alt={project.id} />
+        </div>
+      </div>
+    </div>
+  );
+}
