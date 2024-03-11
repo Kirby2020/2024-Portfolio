828ef0edec73a356288010e04885c5a899dc55a8
Kirby2019 <74135383+Kirby2020@users.noreply.github.com>
2024-03-11 16:20:19 +0100

    build: add debug line for vercel

--- portfolio/src/app/api/projects/projectsController.ts
+++ portfolio/src/app/api/projects/projectsController.ts
@@ -2,21 +2,8 @@ import { Project } from "@prisma/client";
 
 const BASE_URL = process.env.NEXT_PUBLIC_URL + "/api/projects";
 
-// server-side code (e.g., API routes, getServerSideProps, etc.)
-export const getServerSideProps = async (context: any) => {
-  const req = context.req;
-  const domain = req.headers.host; // get the current domain
-
-  const BASE_URL = `http://${domain}/api/projects`; // construct the base URL
-
-  return {
-    props: {
-      BASE_URL,
-    },
-  };
-};
-
 export async function getProjects(): Promise<Project[]> {
+  console.log("fetching projects from " + BASE_URL);
   const res = await fetch(BASE_URL);
 
   if (!res.ok) {
