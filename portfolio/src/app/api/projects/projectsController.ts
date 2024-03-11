f95126ead87d5094591d511e9cd96541d17a3ed9
Kirby2019 <74135383+Kirby2020@users.noreply.github.com>
2024-03-11 17:14:03 +0100

    refactor: Add more debug info for vercel

--- portfolio/src/app/api/projects/projectsController.ts
+++ portfolio/src/app/api/projects/projectsController.ts
@@ -7,7 +7,6 @@ export async function getProjects(): Promise<Project[]> {
   const res = await fetch(BASE_URL);
 
   if (!res.ok) {
-    const error = await res.json();
     // This will activate the closest `error.js` Error Boundary
     throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
   }
