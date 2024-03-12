5d609ca1861cce67668aede3ab0e8f031daf0586
Kirby2019 <74135383+Kirby2020@users.noreply.github.com>
2024-03-12 13:39:12 +0100

    feat: Add extra social icons

--- portfolio/src/app/lib/data/userSocials.ts
+++ portfolio/src/app/lib/data/userSocials.ts
@@ -1,24 +1,50 @@
 export type SocialIcon = {
-    name: string,
-    imageUrl: string,
-    url: string
-}
+  name: string;
+  imageUrl: string;
+  url: string;
+};
 
 const userSocials: SocialIcon[] = [
-    {
-        name: "YouTube",
-        imageUrl: "/images/Youtube_logo.png",
-        url: "https://www.youtube.com/channel/UCu61nUcXdIW1b-qdL2m-Qkw/"
-    },
-    {
-        name: "Twitch",
-        imageUrl: "/images/Twitch_logo.png",
-        url: "https://www.twitch.tv/kirby2016"
-    }, {
-        name: "Spotify",
-        imageUrl: "/images/Spotify_logo.png",
-        url: "https://open.spotify.com/user/31tz4cekt3ukkbngcx7otrdjsv7u?si=b2cbb66247bf46b8"
-    }
+  {
+    name: "LinkedIn",
+    imageUrl: "/images/linkedin_logo.png",
+    url: "#",
+  },
+  {
+    name: "GitHub",
+    imageUrl: "/images/github_logo.png",
+    url: "https://github.com/Kirby2020",
+  },
+  {
+    name: "YouTube",
+    imageUrl: "/images/youtube_logo.png",
+    url: "https://www.youtube.com/channel/UCu61nUcXdIW1b-qdL2m-Qkw/",
+  },
+  {
+    name: "Twitch",
+    imageUrl: "/images/twitch_logo.png",
+    url: "https://www.twitch.tv/kirby2016",
+  },
+  {
+    name: "Spotify",
+    imageUrl: "/images/spotify_logo.png",
+    url: "https://open.spotify.com/user/31tz4cekt3ukkbngcx7otrdjsv7u?si=b2cbb66247bf46b8",
+  },
+  {
+    name: "Pinterest",
+    imageUrl: "/images/pinterest_logo.png",
+    url: "",
+  },
+  {
+    name: "X",
+    imageUrl: "/images/x_logo.png",
+    url: "",
+  },
+  {
+    name: "Discord",
+    imageUrl: "/images/discord_logo.png",
+    url: "",
+  },
 ];
 
 export { userSocials };
